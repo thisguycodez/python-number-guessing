b30d8b69fff5d86fe2f833dd6bf14a5c94d083e1
thisguycodez <58565648+thisguycodez@users.noreply.github.com>
2021-02-14 20:52:25 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -117,7 +117,36 @@ print(random.randint(1,100))
 
 
 
+____________________________________________________________________________________________________________________
+>>> Functions (def) - A block of code that only runs when its called. A function can return data as a result , you can pass data into the function and this is known as a 'parameter': (See more here) https://www.learnpython.org/en/Functions
+---------------------------------------------------------------------------------------------------------------------
 
+
+# A function is declared with the word 'def', followed by the name you choose to use for the function, and then parentheses.- *
+
+def name_of_function():
+	pass
+
+# If you declare a function and do not have code written inside of it yet, then simply add pass statement so the it does not cause errors. Pass is just a null statement, see more here > https://www.educative.io/edpresso/what-is-pass-statement-in-python- *
+
+
+_____________________________________________________________________________________________________________________
+
+
+---------------------------------------------------------------------------------------------------------------------
+
+
+# Ex: Make a function return True ( this funciton now also equals True) especially if this is the only thing it does.
+
+def func():
+	return True
+
+
+_____________________________________________________________________________________________________________________
+
+
+
+# Info - An entire script 'file.py' carries a global scope and a function carries its own within it. More on scopes here > https://www.w3schools.com/python/python_scope.asp
 
 
 
@@ -156,22 +185,32 @@ print(random.randint(1,100))
 
 
 # Try it your self......
-# Answer all the questions below, then move on to test3.py
+# Answer all the questions below, then move on to num_guess.py
 
 
-# 1.) print 3 random numbers between 55-89...
+#1.)print 2 conditions using Comparing operators...
 
 
-# 2.) print the Boolean of 2 random numbers between 1-8 being compared using Comparing operators...:)
+# 2.) print 3 random numbers between 55-89...
 
 
-''' 3.) save 2 random numbers between 15-30 into variables.
- Run an if statement with those 2 variables being compared using
-  Comparing operators as the condition. If True,
-   run "print('This time its true')". 
-   If False, run "print('this time its false')"...:)
+# 3.) print the Boolean of 2 random numbers between 1-8 being compared using Comparing operators...:)
+
+
+# 4.) create a function and add 1-3 inside of it. Call the function.
+
+
+
+
+''' 5.) save 2 random numbers between 15-30 into variables.
+ Run an if statement with those 2 variables being compared 
+ using Comparing operators as the condition. If True, 
+ run "print('This time its true')". If False, 
+ run "print('this time its false')"...:)
 '''
 
+
+# 6 .) create a function and add 5 inside of it. Call the function.
 
 
 
